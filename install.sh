1060eebb606aca53830b591c1ef4a4fb9ae5a40e
Sean Quinn <swquinn@gmail.com>
2014-08-11 22:35:30 -0400

    Checkpoint work.

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR=$ROOT_DIR/base
 ORCL_DIR=$ROOT_DIR/oracle11g
 
+# The location of the Oracle installation files.
+ORAINST_DIR=$BASE_DIR/orainst
+
 IMAGE_TAG="swquinn/oracle-installer"
 RESPONSE_FILE=$BASE_DIR/config/db_install.rsp
 
@@ -30,8 +33,8 @@ done
   
 # Do some validation here, to verify that the user has properly set up their system.
 echo "Verifying dependencies..."
-if [ ! -d /tmp/orainst ]; then
-  echo -e "$(tput setaf 1)Unable to find the installer directory, please create /tmp/orainst$(tput sgr0)"
+if [ ! -d $ORAINST_DIR ]; then
+  echo -e "$(tput setaf 1)Unable to find the installer directory, please create $ORAINST_DIR$(tput sgr0)"
   exit 1
 fi
 
@@ -40,11 +43,12 @@ if [ ! -f $RESPONSE_FILE ]; then
   exit 1
 fi
 
-# Run the docker build
+# Build the docker container
 echo -e "$(tput setaf 7)Building docker image as...$(tput sgr0)"
 echo "  $ docker build -t $IMAGE_TAG --force-rm=true $BASE_DIR"
 docker build -t $IMAGE_TAG --force-rm=true $BASE_DIR
 
-echo -e "$(tput setaf 7)Running oracle installer as...$(tput sgr0)"
-echo "  $ docker run -v "/tmp/orainst:/tmp/orainst" --rm $IMAGE_TAG -silent -force -response /var/opt/oracle/db_install.rsp"
-docker run -v "/tmp/orainst:/tmp/orainst" --rm $IMAGE_TAG -silent -force -response /var/opt/oracle/db_install.rsp
+# Run the docker container
+#echo -e "$(tput setaf 7)Running oracle installer as...$(tput sgr0)"
+#echo "  $ docker run --rm $IMAGE_TAG -silent -noconfig -noprereq -responseFile /var/opt/oracle/db_install.rsp"
+#docker run --rm $IMAGE_TAG -silent -noconfig -noprereq -responseFile "/var/opt/oracle/db_install.rsp"
